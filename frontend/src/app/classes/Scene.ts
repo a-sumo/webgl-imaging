e8b577c0d9206f483e174d1c9b4c78c7e3601888
a-sumo <armandsumo@gmail.com>
2024-01-02 23:15:31 +0100

    add object names

--- frontend/src/app/classes/Scene.ts
+++ frontend/src/app/classes/Scene.ts
@@ -1,23 +1,29 @@
 import { Object3D } from './Object3D';
 
 export class Scene {
-    private objects: Object3D[] = [];
+    private objects: { [name: string]: Object3D } = {};
 
-    // Method to add an object to the scene
-    addObject(object: Object3D) {
-        this.objects.push(object);
+    // Method to add an object to the scene with a name
+    addObject(name: string, object: Object3D) {
+        if (this.objects[name]) {
+            throw new Error(`An object with the name "${name}" already exists in the scene.`);
+        }
+        this.objects[name] = object;
     }
 
-    // Method to remove an object from the scene
-    removeObject(object: Object3D) {
-        const index = this.objects.indexOf(object);
-        if (index > -1) {
-            this.objects.splice(index, 1);
-        }
+
+    // Method to remove an object from the scene by its name
+    removeObject(name: string) {
+        delete this.objects[name];
     }
 
     // Method to get all objects in the scene
     getObjects(): Object3D[] {
-        return this.objects;
+        return Object.values(this.objects);
+    }
+
+    // Method to get an object by its name
+    getObject(name: string): Object3D | undefined {
+        return this.objects[name];
     }
 }
